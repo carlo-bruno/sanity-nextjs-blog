3aca6ea75462c2ac034bb773d01ab044ec1729f1
Carlo Bruno <carlofbruno@gmail.com>
2021-02-10 00:03:56 -0800

    Create post elements to render data from sanity article

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,14 +1,39 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import imageUrlBuilder from '@sanity/image-url';
+import BlockContent from '@sanity/block-content-to-react';
+import postStyles from '../../styles/Post.module.scss';
 
 export const Post = ({ title, body, image }) => {
-  console.log(title, body, image);
-  return <div>This is a post</div>;
+  const [imageUrl, setImageUrl] = useState('');
+
+  useEffect(() => {
+    const imgBuilder = imageUrlBuilder({
+      projectId: 'osrl6g2h',
+      dataset: 'production',
+    });
+
+    setImageUrl(imgBuilder.image(image));
+  }, [image]);
+
+  return (
+    <div>
+      <div className={postStyles.main}>
+        <h2>{title}</h2>
+
+        {imageUrl && (
+          <img className={postStyles.mainImage} src={imageUrl} />
+        )}
+
+        <div className={postStyles.body}>
+          <BlockContent blocks={body} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export const getServerSideProps = async (pageContext) => {
   const pageSlug = pageContext.query.slug;
-
-  console.log(pageSlug);
 
   if (!pageSlug) return { notFound: true };
 
